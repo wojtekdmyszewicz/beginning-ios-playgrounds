648ea53ddbb5a799b87584b09347b4fe555ffcfd
Gabrielle Miller-Messner <gabriellemiller-messner@Gabrielles-MacBook-Pro.local>
2015-10-08 17:16:54 -0700

    adding Dictionaries files

--- Alien Adventure 1/Arrays/Arrays.playground/Pages/Three ways to initialize an array.xcplaygroundpage/Contents.swift
+++ Alien Adventure 1/Arrays/Arrays.playground/Pages/Three ways to initialize an array.xcplaygroundpage/Contents.swift
@@ -7,11 +7,7 @@ var numbers = Array<Double>()
 // More often you will see ...
 var moreNumbers = [Double]()
 
- moreNumbers = [85.0, 90.0, 95.0]
-
 // Array literal syntax
-
-
-  let differentNumbers = [97.5, 98.5, 99.0]
+let differentNumbers = [97.5, 98.5, 99.0]
 
 //: [Next](@next)
