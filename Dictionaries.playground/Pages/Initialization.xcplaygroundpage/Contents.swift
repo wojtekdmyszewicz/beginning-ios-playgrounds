9d7c259ba53b39525182868b6a377ef7fb4858ec
Gabrielle Miller-Messner <gabriellemiller-messner@Gabrielles-MacBook-Pro.local>
2015-10-12 14:27:30 -0700

    editing Dictionaries

--- Dictionaries.playground/Pages/Initialization.xcplaygroundpage/Contents.swift
+++ Dictionaries.playground/Pages/Initialization.xcplaygroundpage/Contents.swift
@@ -8,7 +8,5 @@ var animalGroupsDict = ["whales":"pod", "geese":"flock", "lions": "pride"]
 
 // Another example
 var lifeSpanDict = ["African Grey Parrot": 50...70, "Tiger Salamander": 12...15, "Bottlenose Dolphin": 20...30]
-
 var averageLifeSpanDict = [String:Range<Int>]()
-
 //: [Next](@next)
