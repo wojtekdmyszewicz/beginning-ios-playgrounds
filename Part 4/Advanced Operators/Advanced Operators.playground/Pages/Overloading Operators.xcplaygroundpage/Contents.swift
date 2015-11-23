09125ed731d6f9c4d8e17ba13d327345ae246f39
Jarrod Parkes <jarrod@udacity.com>
2015-11-23 10:59:40 -0600

    Fix loses to losses typos

--- Part 4/Advanced Operators/Advanced Operators.playground/Pages/Overloading Operators.xcplaygroundpage/Contents.swift
+++ Part 4/Advanced Operators/Advanced Operators.playground/Pages/Overloading Operators.xcplaygroundpage/Contents.swift
@@ -5,15 +5,15 @@ We'll create the TeamRecord struct for this example.
 */
 struct TeamRecord {
     var wins: Int
-    var loses: Int
+    var losses: Int
 }
 
-let record_2013 = TeamRecord(wins: 4, loses: 2)
-let record_2014 = TeamRecord(wins: 6, loses: 3)
+let record_2013 = TeamRecord(wins: 4, losses: 2)
+let record_2014 = TeamRecord(wins: 6, losses: 3)
 
 //: Overloading addition operator for TeamRecord
 func + (left: TeamRecord, right: TeamRecord) -> TeamRecord {
-    return TeamRecord(wins: left.wins + right.wins, loses: left.loses + right.loses)
+    return TeamRecord(wins: left.wins + right.wins, losses: left.losses + right.losses)
 }
 
 func +(left: Int, right: Int) -> Int {
@@ -28,7 +28,7 @@ let record_2013_2014 = record_2013 + record_2014
 //: Overloading equality operator for TeamRecord
 func == (left: TeamRecord, right: TeamRecord) -> Bool {
     return (left.wins == right.wins) &&
-        (left.loses == right.loses)
+        (left.losses == right.losses)
 }
 
 record_2013 == record_2014
