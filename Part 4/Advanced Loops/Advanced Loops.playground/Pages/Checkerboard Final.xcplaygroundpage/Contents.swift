a9e146650cadac6d75a84aeee221f011ae25b6bf
Jarrod Parkes <jarrod@udacity.com>
2015-11-25 14:50:15 -0600

    Fix BIOS-231 and add pages and comments to loops

--- Part 4/Advanced Loops/Advanced Loops.playground/Pages/Checkerboard Final.xcplaygroundpage/Contents.swift
+++ Part 4/Advanced Loops/Advanced Loops.playground/Pages/Checkerboard Final.xcplaygroundpage/Contents.swift
@@ -1,18 +1,8 @@
-// Checkerboard con't
-
-for i in 1...8 {
-    for j in 1...8 {
-        if j % 2 == 0 {
-            print("⬛", terminator: "")
-        } else {
-            print("◻️", terminator: "")
-        }
-    }
-    print("")
-}
-
+//: [Previous](@previous)
+/*:
+## Checkerboard Final
+*/
 print("The final answer is below!")
-
 for i in 1...8 {
     for j in 1...8 {
         if j % 2 == i % 2 {
--- Part 4/Advanced Loops/Advanced Loops.playground/Pages/Without Nesting.xcplaygroundpage/Contents.swift
+++ Part 4/Advanced Loops/Advanced Loops.playground/Pages/Without Nesting.xcplaygroundpage/Contents.swift
@@ -1,5 +1,9 @@
-// Checkerboard example
-
+//: [Previous](@previous)
+/*:
+## Without Nesting
+Before we look at nested loops, let's see what a checkboard looks like without nesting...
+*/
+print("Solution without a loop")
 print("⬛◻️⬛◻️⬛◻️⬛◻️")
 print("◻️⬛◻️⬛◻️⬛◻️⬛")
 print("⬛◻️⬛◻️⬛◻️⬛◻️")
@@ -9,8 +13,9 @@ print("◻️⬛◻️⬛◻️⬛◻️⬛")
 print("⬛◻️⬛◻️⬛◻️⬛◻️")
 print("◻️⬛◻️⬛◻️⬛◻️⬛")
 
-
+print("One approach without nesting...")
 for i in 1...4 {
     print("⬛◻️⬛◻️⬛◻️⬛◻️")
     print("◻️⬛◻️⬛◻️⬛◻️⬛")
 }
+//: [Next](@next)
