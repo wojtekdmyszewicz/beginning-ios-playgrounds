525dc50be4722b8612340cdb77ea6e4404b8d8ff
Garrett Holthaus <garrett@udacity.com>
2015-11-27 11:46:15 -0800

    Changed Millileters to Milliliters

--- Part 5/Advanced Structs/Advanced Structs.playground/Pages/Beaker Example Property and Method.xcplaygroundpage/Contents.swift
+++ Part 5/Advanced Structs/Advanced Structs.playground/Pages/Beaker Example Property and Method.xcplaygroundpage/Contents.swift
@@ -4,22 +4,22 @@
 */
 //: ### Beaker Struct
 struct Beaker {
-    let volumeMillileters: Double
+    let volumeMilliliters: Double
     
     // These are US ounces
     var volumeOunces: Double {
         get {
-            return volumeMillileters * 0.033814
+            return volumeMilliliters * 0.033814
         }
     }
     
     func canCountainContents(otherBeaker: Beaker) -> Bool {
-        return volumeMillileters >= otherBeaker.volumeMillileters
+        return volumeMilliliters >= otherBeaker.volumeMilliliters
     }
 }
 
-let beaker1 = Beaker(volumeMillileters: 200)
-let beaker2 = Beaker(volumeMillileters: 500)
+let beaker1 = Beaker(volumeMilliliters: 200)
+let beaker2 = Beaker(volumeMilliliters: 500)
 
 beaker1.volumeOunces
 beaker2.volumeOunces
