9bf3f27c763eeeb451cf39b768efd2411a089866
Wojtek Dmyszewicz <wdmy85@gmail.com>
2016-01-09 22:00:28 +0100

    operators and expressions exercises

--- Part 1/Operators and Expressions/Operators and Expressions Exercises.playground/Contents.swift
+++ Part 1/Operators and Expressions/Operators and Expressions Exercises.playground/Contents.swift
@@ -11,10 +11,24 @@ let twelve = 12
 let twenty = 20
 
 // Desired Value: 4
+
+twelve - twenty + twelve
+
 // Desired Value: -8
+
+twelve - twenty
+
 // Desired Value: 0
+
+five % five
+
 // Desired Value: 100
+
+twenty * five
+
 // Desired Value: 10
+
+five + five
 
 /*:
 ### Exercise 2
@@ -24,20 +38,68 @@ Replace the variable "replaceMe" in the following expressions so that the expres
 */
 var replaceMe = 1
 
-replaceMe % 10          // Desired Value: 6
-replaceMe * replaceMe   // Desired Value: 64
-5 * (replaceMe - 3)     // Desired Value: 105
+16 % 10          // Desired Value: 6
+16 * 4   // Desired Value: 64
+5 * (24 - 3)     // Desired Value: 105
 
 /*:
 ### Exercise 3
 
 Use parentheses to modify the following expressions so that they evaluate to the desired value.
 */
-36 / 3 + 1              // Desired Value: 9
-2 * 5 + 3 * 2 + 4       // Desired Value: 28
-2 * 5 + 3 * 2 + 4       // Desired Value: 96
-144 / 12 - 8 * 1 + 16   // Desired Value: 7
+36 / (3 + 1)              // Desired Value: 9
+(2 * 5) + 3 * (2 + 4)       // Desired Value: 28
+2 * (5 + 3) * (2 + 4)       // Desired Value: 96
+144 / (12 - 8 * 1 + 16)   // Desired Value: 7
 
+
+// Testing
+var x = 20
+var y = 4
+var z = 13
+
+var t = 20
+var m = 4
+
+2 * 9
+
+18 % 2
+
+var result1 = 4 + 2 * 9 % 2
+
+20 * 4
+
+var c = ++t
+m++
+
+21 * 4
+
+var result2 = ++x * y++
+y
+
+var result3 = z / 2 + 4 * 2
+
+z / 2
+
+4 * 2
+
+6 + 8
+
+var isWeekend = true;
+
+var isRaining = false;
+
+var isHot = false;
+
+var stayingInside = false;
+
+var stayAtHome = isWeekend && isRaining
+
+var wearWarmClothes = !isHot && !stayingInside
+
+var numberOfLives = 0;
+
+var shouldContinue = numberOfLives <= 0
 
 
 
