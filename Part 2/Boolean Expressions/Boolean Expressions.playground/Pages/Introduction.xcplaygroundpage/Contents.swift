7517a2bb041d5e4d8536230b25b9442aef13a986
Wojtek Dmyszewicz <Wojtek@Macintosh.local>
2016-02-16 00:57:29 +0100

    playing around with booleans

--- Part 2/Boolean Expressions/Boolean Expressions.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
+++ Part 2/Boolean Expressions/Boolean Expressions.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
@@ -3,7 +3,34 @@
 import UIKit
 
 // Introduction to Boolean Expressions
-var age = 5
+//var age = 5
+var age = 29
+let applySeniorDiscount = age >= 65
+
 var timeForKindergarten = age == 5
 var canVote = age >= 18
+
+1.0 == 1
+
+struct Rectangle {
+    let height : Int
+    let width : Int
+}
+
+let rect1 = Rectangle(height: 7, width: 5)
+let rect2 = Rectangle(height: 5, width: 7)
+
+//rect1 == rect2
+
+let arrayOfInts = [10,8,9]
+let biggerArrayOfInts = [6,5,4,3]
+
+//let arraySizeBool = arrayOfInts.count
+
+let arraySizeBool = arrayOfInts.count < biggerArrayOfInts.count
+
+let anotherAlphabeticalBool = "xerox" > "xylophone"
+
+
+
 //: [Next](@next)
